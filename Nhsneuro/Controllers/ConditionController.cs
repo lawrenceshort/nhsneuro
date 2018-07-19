5eaa658a6994be1b4a68eeb2750c1030bbba7a23
lawrence.short <lawrence.short@careflowconect.com>
2018-07-19 11:25:15 +0100

    removed unused methods from conditioncontroller. updated Index.cshtl file to display the values of the selected symptoms.

--- Nhsneuro/Controllers/ConditionController.cs
+++ Nhsneuro/Controllers/ConditionController.cs
@@ -6,8 +6,6 @@ using System.Web.Mvc;
 
 namespace Nhsneuro.Controllers
 {
-    using Models;
-
     public class ConditionController : Controller
     {
         public ConditionController()
@@ -25,77 +23,5 @@ namespace Nhsneuro.Controllers
 
             return View(ViewData);
         }
-
-        //// GET: Condition/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
-
-        //// GET: Condition/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Condition/Create
-        //[HttpPost]
-        //public ActionResult Create(FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Condition/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Condition/Edit/5
-        //[HttpPost]
-        //public ActionResult Edit(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Condition/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Condition/Delete/5
-        //[HttpPost]
-        //public ActionResult Delete(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
 }
